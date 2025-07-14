eec9bb1b777561203d25c6e12714184ba519a866
ajcur <173679835+ajcur@users.noreply.github.com>
2025-07-14 16:27:32 +0200

    complete exercise 09

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function(firstNum, lastNum) {
+    let smallerNum;
+    let largerNum;
+    if (firstNum < 0 || lastNum < 0 || !Number.isInteger(firstNum) || !Number.isInteger(lastNum)) {
+        return 'ERROR';
+    }
+    if (firstNum < lastNum) {
+        smallerNum = firstNum;
+        largerNum = lastNum;
+    } else {
+        smallerNum = lastNum;
+        largerNum = firstNum;
+    }
+    let sum = 0;
+    for (let i = smallerNum; i <= largerNum; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
