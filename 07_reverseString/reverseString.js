ec4c42daee549d50d7a31508d85d990fb7b35186
ajcur <173679835+ajcur@users.noreply.github.com>
2025-07-14 13:30:28 +0200

    complete exercise 07

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let array = string.split('');
+    let newArray = array.toReversed();
+    return newString = newArray.reduce((total, letter) => {
+        return total + letter;
+    }, '');
 };
 
 // Do not edit below this line
