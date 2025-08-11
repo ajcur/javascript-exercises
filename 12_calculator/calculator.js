82304eefe24049ef9d1e1f526052eb500c818b2e
ajcur <173679835+ajcur@users.noreply.github.com>
2025-08-11 15:42:14 +0200

    Complete exercise 12

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(firstNumber, secondNumber) {
+	result = firstNumber + secondNumber;
+  return result;
 };
 
-const subtract = function() {
-	
+const subtract = function(firstNumber, secondNumber) {
+	result = firstNumber - secondNumber;
+  return result;
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+	result = numbers.reduce((result, number) => result + number, 0);
+  return result;
 };
 
-const multiply = function() {
-
+const multiply = function(numbers) {
+  result = numbers.reduce((result, number) => result * number, 1);
+  return result;
 };
 
-const power = function() {
-	
+const power = function(number, exponent) {
+	let result = 1;
+  for (let i = 0; i < exponent; i++) {
+    result = result * number;
+  }
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+	let result = 1;
+  for (let i = number; i > 0; i--) {
+    result = result * i;
+  }
+  return result;
 };
 
 // Do not edit below this line
