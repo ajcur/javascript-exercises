39fdb318597d871e2a6381a05dbcae7a205a10bf
ajcur <173679835+ajcur@users.noreply.github.com>
2025-08-11 16:42:06 +0200

    Complete exercise 14

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(index) {
+    if (index < 0) {
+        return "OOPS";
+    }
+    numbers = [0, 1, 1]
+    for (i = 3; i <= index; i++) {
+        number = numbers[i-1] + numbers[i-2];
+        numbers.push(number);
+    }
+    return numbers[index];
 };
 
 // Do not edit below this line
