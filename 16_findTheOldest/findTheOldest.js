fec6e6e6681802e8c1db9cbc9c3afef31c689d5a
ajcur <173679835+ajcur@users.noreply.github.com>
2025-08-11 16:58:05 +0200

    Complete exercise 16

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    for (person of people) {
+        if (person.yearOfDeath) {
+            person.age = person.yearOfDeath - person.yearOfBirth;
+        } else {
+            let currentDate = new Date().getFullYear();
+            person.age = currentDate - person.yearOfBirth;
+        }
+    }
+    people.sort((a, b) => {
+        if (a.age > b.age) {
+            return -1;
+        } else return 1;
+    })
+    return people[0];
 };
 
 // Do not edit below this line
