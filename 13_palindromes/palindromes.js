5a68e9c0ef4a78f73963e2bde31e1edeac307a7b
ajcur <173679835+ajcur@users.noreply.github.com>
2025-08-11 16:18:59 +0200

    Complete exercise 13

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,28 @@
-const palindromes = function () {
+const palindromes = function (phrase) {
+/*  let array = phrase.split("");
+    let cleanedArray = array.filter((char) => /[A-Za-z0-9]/.test(char));
+    let lowerCaseArray = cleanedArray.map((char) => char.toLowerCase());
+    let reversedArray = lowerCaseArray.toReversed();
+    let cleanedPhrase = lowerCaseArray.join("");
+    let reversedPhrase = reversedArray.join("");
 
+    return reversedPhrase == cleanedPhrase; */
+
+    let cleanedPhrase = phrase
+        .toLowerCase()
+        .split('')
+        .filter((char) => /[a-z0-9]/.test(char))
+        .join('');
+
+    let reversedPhrase = cleanedPhrase
+        .split('')
+        .reverse()
+        .join('');
+
+    return reversedPhrase == cleanedPhrase;
 };
+
+palindromes("racecar");
 
 // Do not edit below this line
 module.exports = palindromes;
